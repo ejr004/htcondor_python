b9a6a3e03d31f8c50577c3a15a12a8e2c28240df
ejr004 <acessoej@icloud.com>
2020-08-14 21:46:21 -0300

    first commit

--- main.py
+++ main.py
@@ -2,12 +2,7 @@ import htcondor
 import classad
 import socket # We'll use this to automatically fill in our hostname
 
-name = classad.quote(f"jovyan@{socket.getfqdn()}")
-coll.query(
-    htcondor.AdTypes.Schedd, 
-    constraint=f"Name =?= {name}", 
-    projection=["Name", "MyAddress", "DaemonCoreDutyCycle"],
-)
+
 schedd = htcondor.Schedd(schedd_ad)
 print(schedd)
 
@@ -15,3 +10,29 @@ coll = htcondor.Collector()  # create the object representing the collector
 schedd_ad = coll.locate(htcondor.DaemonTypes.Schedd) # locate the default schedd
 
 print(schedd_ad)
+
+# 1: Idle (I)
+# 2: Running (R)
+# 3: Removed (X)
+# 4: Completed (C)
+# 5: Held (H)
+# 6: Transferring Output
+# 7: Suspended
+
+for job in schedd.xquery(projection=['x509UserProxyVOName',
+                                     'x509UserProxyEmail',
+                                     'ClusterId',
+                                     'JobStatus',]):
+    print(repr(job))
+#
+# for job in schedd.xquery(
+#         projection=['x509UserProxyVOName',
+#                     'ClusterId',
+#                     'JobStatus',]):
+#         print(repr(job))
+
+# results = schedd.query('JobStatus =?= "JobStatus = 4"', ["ClusterId", "ProcId"])
+# len(results)
+
+# for job in schedd.query(attr_list=['JobStatus']):
+#     print(repr(job))
